8c1aaeec4c63e2a5be0c8eb8062a660c893af515
Mehdi Zerrad <mehdi@1v1euchre.com>
2026-07-09 13:58:12 -0400

    Add public Discord invite

--- src/lib/downloadLinks.js
+++ src/lib/downloadLinks.js
@@ -3,7 +3,7 @@ export const APP_STORE_EUCHRE_URL = '';
 export const WEB_SPADES_URL = 'https://1v1spades.com';
 export const WEB_EUCHRE_URL = 'https://1v1euchre.com';
 export const TOURNAMENTS_URL = 'https://1v1tournaments.org';
-export const DISCORD_URL = '';
+export const DISCORD_URL = 'https://discord.gg/WUmaXz7X6';
 export const TWITCH_URL = 'https://www.twitch.tv/1v1compspades';
 export const YOUTUBE_URL = 'https://m.youtube.com/channel/UCkqnaYQ2I47O8e20sIsHpfQ?ra=m';
 
