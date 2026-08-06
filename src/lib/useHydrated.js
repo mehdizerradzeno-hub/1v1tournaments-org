2209f0e66f749a5adf5e8743fea3df7c5b7c92eb
Mehdi Z <mehdiz@tests-mbp.lan>
2026-08-06 13:38:09 -0400

    fix(web): defer league bootstrap past hydration

--- src/lib/useHydrated.js
+++ src/lib/useHydrated.js
@@ -8,31 +8,14 @@ export function useHydrated() {
       return undefined;
     }
 
-    let idleId = 0;
     let timeoutId = 0;
-    let frameId = 0;
-
-    const markHydrated = () => {
-      frameId = window.requestAnimationFrame(() => {
-        setHydrated(true);
-      });
-    };
-
-    if (typeof window.requestIdleCallback === 'function') {
-      idleId = window.requestIdleCallback(markHydrated, { timeout: 1500 });
-    } else {
-      timeoutId = window.setTimeout(markHydrated, 250);
-    }
+    timeoutId = window.setTimeout(() => {
+      setHydrated(true);
+    }, 2000);
 
     return () => {
-      if (idleId && typeof window.cancelIdleCallback === 'function') {
-        window.cancelIdleCallback(idleId);
-      }
       if (timeoutId) {
         window.clearTimeout(timeoutId);
-      }
-      if (frameId) {
-        window.cancelAnimationFrame(frameId);
       }
     };
   }, []);
