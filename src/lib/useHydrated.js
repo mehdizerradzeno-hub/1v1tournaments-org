acba6fcf34a666b2819e44071726f623c3198dcd
Mehdi Z <mehdiz@tests-mbp.lan>
2026-08-06 12:52:15 -0400

    fix(web): delay client hydration updates on league pages

--- src/lib/useHydrated.js
+++ src/lib/useHydrated.js
@@ -1,6 +1,11 @@
 import { useSyncExternalStore } from 'react';
 
-function subscribe() {
+function subscribe(callback) {
+  if (typeof window !== 'undefined' && typeof window.requestAnimationFrame === 'function') {
+    const frame = window.requestAnimationFrame(() => callback());
+    return () => window.cancelAnimationFrame(frame);
+  }
+
   return () => {};
 }
 
