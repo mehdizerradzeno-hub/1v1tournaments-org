42676e24d821f7ea9bf589a6c8f570a2e03f2d7b
Mehdi Zerrad <mehdizerradzeno@gmail.com>
2026-08-18 08:25:54 -0400

    Fix tournaments mobile zoom scrolling

--- src/screens/TournamentLandingRoute.jsx
+++ src/screens/TournamentLandingRoute.jsx
@@ -1,12 +1,6 @@
-import { View } from 'react-native';
-
 import NextScreen from './NextScreen';
 import '../styles/tournamentResponsive.css';
 
 export default function TournamentLandingRoute() {
-  return (
-    <View dataSet={{ tournamentPage: 'true' }} style={{ width: '100%', minWidth: 0, maxWidth: '100%' }}>
-      <NextScreen />
-    </View>
-  );
+  return <NextScreen />;
 }
